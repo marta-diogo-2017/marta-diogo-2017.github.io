8c2a40c5e55500999259ae7df590087bc913eddc
St K <claudiacvlho@gmail.com>
2017-05-07 22:32:18 +0100

    ADD: link to parede

--- js/modules/components/_website.js
+++ js/modules/components/_website.js
@@ -192,12 +192,12 @@ class Website extends React.Component {
             <div className="col-xs-12 col-sm-6 margin-bottom-lol">
               <img src="/assets/img/igrejaParede.jpg" className="img-responsive" />
               <p className="legend">Avenida Amadeu Duarte, 514, 2775 Parede</p>
-              <p><a href="">mapa aqui</a></p>
+              <p><a href="https://goo.gl/maps/ucFxkJNWc852" target="_blank">mapa aqui</a></p>
             </div>
             <div className="col-xs-12 col-sm-6 margin-bottom-lol">
               <img src="/assets/img/quintaMurta.jpg" className="img-responsive" />
               <p className="legend">Estr. Velha do Boição, 632, 2670 Bucelas</p>
-              <p><a href="https://goo.gl/maps/BRTrs9c8gvn">mapa aqui</a></p>
+              <p><a href="https://goo.gl/maps/BRTrs9c8gvn" target="_blank">mapa aqui</a></p>
               {/*<iframe className="maps__mapa" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3103.8263184680463!2d-9.129081084648762!3d38.927947079565996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd192bb53d9f367f%3A0x8adecf65a99496a0!2sQuinta+da+Murta!5e0!3m2!1sen!2spt!4v1494182016637" frameborder="0" style="border:0" allowfullscreen></iframe>*/}
             </div>
           </section>
